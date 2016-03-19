4abb9db85ca78c027a495bf6039b9002657deebf
COOLib <COOLib@yandex.ru>
2016-03-19 13:05:29 +0200

    Test for the executor's implementation

--- Module2.Generics/test/ExecutorImplTest.java
+++ Module2.Generics/test/ExecutorImplTest.java
@@ -8,12 +8,19 @@ public class ExecutorImplTest {
     @Test
     public void SomeTest() throws Exception {
 
-        List<Rectangle> rectangles = new ArrayList<>();
+        List<Number> squares = new ArrayList<>();
 
         ExecutorImpl numberExecutor = new ExecutorImpl();
+        Validator validator = new NumberValidator();
 
-        numberExecutor.addTask(new Rectangle(10, 20), new NumberValidator());
-        numberExecutor.addTask(new Rectangle(0, 0), new NumberValidator());
+        numberExecutor.addTask(new Rectangle(10, 20), validator);
+        numberExecutor.addTask(new Rectangle(0, 0), validator);
+        numberExecutor.addTask(new Rectangle(20, 30), validator);
+        numberExecutor.addTask(new Rectangle(15, 15), validator);
+        numberExecutor.addTask(new Rectangle(10, 8), validator);
+        numberExecutor.addTask(new Circle(10), validator);
+        numberExecutor.addTask(new Circle(7), validator);
+
 
         numberExecutor.execute();
 
