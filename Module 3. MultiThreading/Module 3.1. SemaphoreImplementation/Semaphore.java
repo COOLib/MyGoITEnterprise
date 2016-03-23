695dfad03ed61538ffb31902d87ab07d222e5782
COOLib <COOLib@yandex.ru>
2016-03-23 19:09:12 +0200

    Implementation of the Semaphore's interface

---
+++ Module 3. MultiThreading/Module 3.1. SemaphoreImplementation/Semaphore.java
@@ -0,0 +1,13 @@
+
+public interface Semaphore {
+
+    public void acquire() throws InterruptedException;
+
+    public void acquire(int permits) throws InterruptedException;
+
+    public void release() throws InterruptedException;
+
+    public void release(int permits);
+
+    public int getAvailablePermits();
+}
