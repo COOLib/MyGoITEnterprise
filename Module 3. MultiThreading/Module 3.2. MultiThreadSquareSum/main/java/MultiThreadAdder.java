77dfd98f65b63be0fcc39b9a736f466dbc511d10
COOLib <COOLib@yandex.ru>
2016-04-01 16:07:56 +0300

    Sum of squares of an array's elements

--- Module 3. MultiThreading/Module 3.2. MultiThreadSquareSum/main/java/MultiThreadAdder.java
+++ Module 3. MultiThreading/Module 3.2. MultiThreadSquareSum/main/java/MultiThreadAdder.java
@@ -5,10 +5,15 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class MultiThreadAdder implements SquareSum {
 
+    private long singleThreadTime;
+    private long multiThreadTime;
+    private long[] multiArr;
+    private long[] singleArr;
+
     public static void main(String[] args) {
 
         MultiThreadAdder adder = new MultiThreadAdder();
-        int[] array = new int[100];
+        int[] array = new int[150];
 
         for (int i = 0; i < array.length; i++) {
             array[i] = i * i + 1;
@@ -16,17 +21,56 @@ public class MultiThreadAdder implements SquareSum {
         int numberOfThreads = array.length / 10;
         adder.getSquareSum(array, numberOfThreads);
 
-        System.out.println(adder.getSquareSum(array, numberOfThreads));
-        System.out.println(adder.getSum(array));
+        System.out.println("Square sum counted by single thread: " + adder.getSum(array, numberOfThreads));
+        System.out.println("Square sum counted by " + numberOfThreads + " threads: " + adder.getSquareSum(array, numberOfThreads));
+        System.out.println();
+        System.out.println("Required time for single thread(in nanoseconds): " + adder.singleThreadTime);
+        System.out.println("Required time for " + numberOfThreads + " threads(in nanoseconds): " + adder.multiThreadTime);
+        System.out.println();
+        for (int i = 0; i < adder.multiArr.length; i++) {
+            System.out.print(adder.multiArr[i] + "  ");
+        }
+        System.out.println();
+        for (int i = 0; i < adder.singleArr.length; i++) {
+            System.out.print(adder.singleArr[i] + "  ");
+        }
     }
 
-    public long getSum(int[] values) {
+    public long getSum(int[] values, int numberOfThreads) {
 
         long square = 0;
+        long a = System.nanoTime();
+        singleArr = new long[numberOfThreads];
 
-        for (int i = 0; i < values.length; i++) {
-            square += values[i] * values[i];
+        if (values.length < numberOfThreads) {
+
+            for (int i = 0; i < values.length; i++) {
+                singleArr[i] = values[i] * values[i];
+                square += values[i] * values[i];
+            }
+        } else {
+            for (int k = 0; k < numberOfThreads; k++) {
+
+                int start = (values.length / numberOfThreads) * k;
+                int end = (values.length / numberOfThreads) * (k + 1);
+
+                for (int j = start; j < end; j++) {
+                    square += values[j] * values[j];
+                    singleArr[k] += values[j] * values[j];
+                }
+
+                if ((k == numberOfThreads - 1) && end < values.length) {
+
+                    for (int m = end; m < values.length; m++) {
+                        square += values[m] * values[m];
+                        singleArr[k] += values[m] * values[m];
+                    }
+                }
+            }
         }
+        long b = System.nanoTime();
+
+        singleThreadTime = b - a;
 
         return square;
     }
@@ -34,40 +78,47 @@ public class MultiThreadAdder implements SquareSum {
     public long getSquareSum(int[] values, int numberOfThreads) {
 
         AtomicLong k = new AtomicLong();
+        multiArr = new long[numberOfThreads];
 
         ExecutorService executor = Executors.newFixedThreadPool(numberOfThreads);
         Phaser phaser = new Phaser(numberOfThreads);
 
+        long a = System.nanoTime();
+
         for (int i = 0; i < numberOfThreads; i++) {
             final int finalI = i;
-            executor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    int square = 0;
+            if (finalI == values.length) {
+                break;
+            }
+            executor.execute(() -> {
+                int square = 0;
 
-                    if (values.length < numberOfThreads) {
-                        square = values[finalI] * values[finalI];
-                    } else {
-                        int start = (values.length / numberOfThreads) * finalI;
-                        int end = (values.length / numberOfThreads) * (finalI + 1);
+                if (values.length < numberOfThreads) {
+                    square = values[finalI] * values[finalI];
+                    multiArr[finalI] = values[finalI] * values[finalI];
 
-                        for (int j = start; j < end; j++) {
-                            square += values[j] * values[j];
-                        }
+                } else {
+                    int start = (values.length / numberOfThreads) * finalI;
+                    int end = (values.length / numberOfThreads) * (finalI + 1);
 
-                        if ((finalI == numberOfThreads - 1) && end < values.length) {
+                    for (int j = start; j < end; j++) {
+                        square += values[j] * values[j];
+                        multiArr[finalI] += values[j] * values[j];
+                    }
 
-                            for (int m = end; m < values.length; m++) {
-                                square += values[m] * values[m];
-                            }
+                    if ((finalI == numberOfThreads - 1) && end < values.length) {
+
+                        for (int m = end; m < values.length; m++) {
+                            square += values[m] * values[m];
+                            multiArr[finalI] += values[m] * values[m];
                         }
                     }
-                    //String name = Thread.currentThread().getName();
-                    //System.out.println(name + " starts waiting.");
-                    phaser.arriveAndAwaitAdvance();
-                    k.addAndGet(square);
-                    //System.out.println(name + " finishes waiting.");
                 }
+                //String name = Thread.currentThread().getName();
+                //System.out.println(name + " starts waiting.");
+                phaser.arriveAndAwaitAdvance();
+                k.addAndGet(square);
+                //System.out.println(name + " finishes waiting.");
             });
             try {
                 Thread.sleep(100);
@@ -75,6 +126,11 @@ public class MultiThreadAdder implements SquareSum {
                 e.printStackTrace();
             }
         }
+
+        long b = System.nanoTime();
+
+        multiThreadTime = b - a;
+
         executor.shutdown();
 
         return k.longValue();
