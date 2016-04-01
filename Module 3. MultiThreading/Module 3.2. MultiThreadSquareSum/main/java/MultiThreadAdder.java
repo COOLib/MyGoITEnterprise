9a85a08f3377ce9237d6d666a0f9615c60ac8174
COOLib <COOLib@yandex.ru>
2016-04-01 23:46:52 +0300

    Sum of squares of an array's elements

--- Module 3. MultiThreading/Module 3.2. MultiThreadSquareSum/main/java/MultiThreadAdder.java
+++ Module 3. MultiThreading/Module 3.2. MultiThreadSquareSum/main/java/MultiThreadAdder.java
@@ -1,46 +1,32 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Phaser;
-import java.util.concurrent.atomic.AtomicLong;
+
 
 public class MultiThreadAdder implements SquareSum {
-
-    private long singleThreadTime;
-    private long multiThreadTime;
-    private long[] multiArr;
-    private long[] singleArr;
 
     public static void main(String[] args) {
 
         MultiThreadAdder adder = new MultiThreadAdder();
-        int[] array = new int[150];
+        int[] array = new int[200];
 
         for (int i = 0; i < array.length; i++) {
             array[i] = i * i + 1;
         }
         int numberOfThreads = array.length / 10;
-        adder.getSquareSum(array, numberOfThreads);
 
-        System.out.println("Square sum counted by single thread: " + adder.getSum(array, numberOfThreads));
-        System.out.println("Square sum counted by " + numberOfThreads + " threads: " + adder.getSquareSum(array, numberOfThreads));
+        long singleSum = adder.getSum(array,numberOfThreads);
+        System.out.println("Square sum counted by single thread: " + singleSum);
+
         System.out.println();
-        System.out.println("Required time for single thread(in nanoseconds): " + adder.singleThreadTime);
-        System.out.println("Required time for " + numberOfThreads + " threads(in nanoseconds): " + adder.multiThreadTime);
-        System.out.println();
-        for (int i = 0; i < adder.multiArr.length; i++) {
-            System.out.print(adder.multiArr[i] + "  ");
-        }
-        System.out.println();
-        for (int i = 0; i < adder.singleArr.length; i++) {
-            System.out.print(adder.singleArr[i] + "  ");
-        }
+        long multiSum = adder.getSquareSum(array, numberOfThreads);
+        System.out.println("Square sum counted by " + numberOfThreads + " threads: " + multiSum);
     }
 
     public long getSum(int[] values, int numberOfThreads) {
 
         long square = 0;
-        long a = System.nanoTime();
-        singleArr = new long[numberOfThreads];
+        long[] singleArr = new long[numberOfThreads];
 
         if (values.length < numberOfThreads) {
 
@@ -68,33 +54,31 @@ public class MultiThreadAdder implements SquareSum {
                 }
             }
         }
-        long b = System.nanoTime();
 
-        singleThreadTime = b - a;
+        for (int i = 0; i < singleArr.length; i++) {
+            System.out.print(singleArr[i] + "  ");
+        }
+        System.out.println();
 
         return square;
     }
 
     public long getSquareSum(int[] values, int numberOfThreads) {
 
-        AtomicLong k = new AtomicLong();
-        multiArr = new long[numberOfThreads];
+        long[] multiArr = new long[numberOfThreads];
 
         ExecutorService executor = Executors.newFixedThreadPool(numberOfThreads);
         Phaser phaser = new Phaser(numberOfThreads);
 
-        long a = System.nanoTime();
-
         for (int i = 0; i < numberOfThreads; i++) {
             final int finalI = i;
+
             if (finalI == values.length) {
                 break;
             }
             executor.execute(() -> {
-                int square = 0;
 
                 if (values.length < numberOfThreads) {
-                    square = values[finalI] * values[finalI];
                     multiArr[finalI] = values[finalI] * values[finalI];
 
                 } else {
@@ -102,37 +86,45 @@ public class MultiThreadAdder implements SquareSum {
                     int end = (values.length / numberOfThreads) * (finalI + 1);
 
                     for (int j = start; j < end; j++) {
-                        square += values[j] * values[j];
                         multiArr[finalI] += values[j] * values[j];
                     }
 
                     if ((finalI == numberOfThreads - 1) && end < values.length) {
 
                         for (int m = end; m < values.length; m++) {
-                            square += values[m] * values[m];
                             multiArr[finalI] += values[m] * values[m];
                         }
                     }
                 }
-                //String name = Thread.currentThread().getName();
-                //System.out.println(name + " starts waiting.");
                 phaser.arriveAndAwaitAdvance();
-                k.addAndGet(square);
-                //System.out.println(name + " finishes waiting.");
             });
             try {
-                Thread.sleep(100);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
-        long b = System.nanoTime();
-
-        multiThreadTime = b - a;
-
         executor.shutdown();
 
-        return k.longValue();
+
+        for (int i = 0; i < multiArr.length; i++) {
+            System.out.print(multiArr[i] + "  ");
+        }
+        System.out.println();
+
+        long result = addingForSum(multiArr);
+
+        return result;
+    }
+
+    private long addingForSum (long[] subResults) {
+
+        long result = 0;
+
+        for (int i = 0; i < subResults.length; i++) {
+            result += subResults[i];
+        }
+
+        return result;
     }
 }
