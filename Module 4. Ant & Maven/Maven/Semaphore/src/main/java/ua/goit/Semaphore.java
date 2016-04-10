ba3f35eefbbe9c65b0b34cc871fdcdb886b1e465
COOLib <COOLib@yandex.ru>
2016-04-11 00:32:25 +0300

    all source files for this project

---
+++ Module 4. Ant & Maven/Maven/Semaphore/src/main/java/ua/goit/Semaphore.java
@@ -0,0 +1,14 @@
+package ua.goit;
+
+public interface Semaphore {
+
+    public void acquire() throws InterruptedException;
+
+    public void acquire(int permits) throws InterruptedException;
+
+    public void release() throws InterruptedException;
+
+    public void release(int permits);
+
+    public int getAvailablePermits();
+}
