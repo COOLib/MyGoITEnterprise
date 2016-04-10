57e1f483018ab6e12267dceb9f071cc2cc0511b3
COOLib <COOLib@yandex.ru>
2016-04-10 23:48:07 +0300

    Test files

---
+++ Module 4. Ant & Maven/Maven/Collection Efficiency/src/test/java/ua/java/goit/MyWriterTest.java
@@ -0,0 +1,13 @@
+package ua.java.goit;
+
+import org.junit.Test;
+
+public class MyWriterTest {
+
+    @Test
+    public void testOutToDisplay() throws Exception {
+
+        MyWriter writer = new MyWriter();
+        writer.outToDisplay();
+    }
+}
