91b1a36e5723259fcebb3d3e0c8c242f30f0599a
COOLib <COOLib@yandex.ru>
2016-04-11 09:48:35 +0300

    test file

---
+++ Module 4. Ant & Maven/Maven/Phaser/src/test/java/ua/goit/MultiThreadAdderTest.java
@@ -0,0 +1,38 @@
+package ua.goit;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class MultiThreadAdderTest {
+
+    private final static int[] array = new int[100];
+    private final static int numberOfThreads = 10;
+    private static long squareSum;
+    private final MultiThreadAdder adder = new MultiThreadAdder();
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i * i;
+            squareSum += array[i] * array[i];
+        }
+    }
+
+    @Test
+    public void testGetSum() throws Exception {
+
+        final long result = adder.getSum(array, numberOfThreads);
+        assertThat(result, is(squareSum));
+
+    }
+
+    @Test
+    public void testGetSquareSum() throws Exception {
+
+        final long result = adder.getSquareSum(array, numberOfThreads);
+        assertThat(result, is(squareSum));
+    }
+}
