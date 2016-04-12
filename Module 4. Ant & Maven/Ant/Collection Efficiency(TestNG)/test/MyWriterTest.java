75478dbc00ea8823c171b162d955ebee9d1f46ac
COOLib <COOLib@yandex.ru>
2016-04-12 14:14:52 +0300

    test files

---
+++ Module 4. Ant & Maven/Ant/Collection Efficiency(TestNG)/test/MyWriterTest.java
@@ -0,0 +1,9 @@
+public class MyWriterTest {
+
+    @org.testng.annotations.Test
+    public void testOutToDisplay() throws Exception {
+
+        MyWriter writer = new MyWriter();
+        writer.outToDisplay();
+    }
+}
